3db6ae22ac8295d6a1655e85182bf625b69e3865
kelvinleonardos <kelvinlsianipar@gmail.com>
2023-03-19 14:43:27 +0800

    upgit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function myFunction() {
     var x = document.getElementsByClassName("nav-links-container");
     Array.from(x).forEach((elm) => {
         if (elm.style.display === "none") {
-          elm.style.display = "block";
+          elm.style.display = "flex";
         } else {
           elm.style.display = "none";
         }
